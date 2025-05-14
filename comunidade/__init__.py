d5b3adb2d01903d7c0015f1a28fee4405761a16e
thfaria <thfaria@gmail.com>
2025-05-13 23:28:30 -0300

    Update __init__.py
    Inclusão de código para verificação da existência do banco de dados e criação do mesmo em caso negativo

--- comunidade/__init__.py
+++ comunidade/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
+
 
 # Configurações de APP
 app = Flask(__name__)
@@ -23,6 +25,20 @@ login_manager = LoginManager(app) # Administração da parte de login do
 login_manager.login_view = 'login' # Indica para onde o usuário deve ser redirecionado caso tente acessar alguma página exclusive para quem esta logado sem estar
 login_manager.login_message = 'Você precisa estar logado para acessar essa página' # Mensagem que será apresentada para o usuário caso ele não esteja logado e tente acessar uma página exclusiva
 login_manager.login_message_category = 'alert-info' # Categoria da mensagem que será apresentada para o usuário
+
+
+# Processo para verificar se a base de dados já existe no ambinete de produção
+from comunidade import models # Importa os modelos das tabelas
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI']) # Criação de uma engine a partir do caminho para o banco de dados
+inspector = sqlalchemy.inspect(engine) # Criação de um inspetor para verificar a existência de tabelas no banco de dados
+
+if not inspector.has_table('usuario'): # Verifica se a tabela de usuários não existe no banco (poderia ser qualquer tabela aqui)
+    with app.app_context(): # Dentro do contexto da aplicação
+        database.drop_all() # Deleta qualquer base previamente existente
+        database.create_all() # Cria a base com as tabelas dos models
+        print('Base de dados criada!')
+else:
+    print('Base de dados já existe!')
 
 
 # Após configuração do app e definição do banco, cha-se o arquivo com a definição dos Routes que de fato vão criar o site
